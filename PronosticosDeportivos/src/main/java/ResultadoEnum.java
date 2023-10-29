92f661b4ef443b031c457e421fbfbdab76f40f20
Federico Martin <fedesmartin@live.com>
2023-10-29 12:43:57 -0300

    Se crea proyecto Maven y se agregan archivos CSV

---
+++ PronosticosDeportivos/src/main/java/ResultadoEnum.java
@@ -0,0 +1,4 @@
+
+public enum ResultadoEnum {
+	GANADOR,PERDEDOR,EMPATE
+}
---
+++ PronosticosDeportivos/src/main/java/Ronda.java
@@ -0,0 +1,14 @@
+
+public class Ronda {
+	
+	private String nro;
+
+	public Ronda(String nro) {
+		this.nro = nro;
+	}
+
+	public String getNro() {
+		return nro;
+	}	
+
+}
